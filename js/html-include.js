b9ff41a3f770913f2ce3a1877d1b0860c50a7ec6
Geoff Lyman <gtdesigns27@gmail.com>
2021-02-04 22:53:00 -0500

    Updating partial import js

---
+++ js/html-include.js
@@ -0,0 +1,26 @@
+# -- ./js/html-include.js --
+
+class HTMLInclude extends HTMLElement {
+    constructor(src) {
+        super();
+        this.attachShadow({mode: "open"});
+        if (src) { 
+            this.textContent = src;
+        }
+        setTimeout(() => this._load());
+    }
+    async _load() {
+        let src = this.textContent.trim();
+
+        if (!src) {
+            throw new Error("URL missing between <html-import> tags.");
+        } 
+        let rsp = await fetch(src);
+
+        if (rsp.status != 200) {
+            throw new Error(`Failed to load file (${src}) for <html-import>.`);
+        }
+        this.shadowRoot.innerHTML = await rsp.text();
+    }
+}
+customElements.define("html-include", HTMLInclude);
