61a91a566489d2d9be75b29f95b2ecee7d3fbd21
glyman01 <hey@geofflyman.com>
2025-05-06 20:23:56 -0400

    tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
- // tailwind.config.js
 module.exports = {
-	purge: [
-	 // './src/**/*.html',
-	 // './src/**/*.js',
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		fontFamily: {
-			sans: ['Helvetica Neue', 'sans-serif'],
-			serif: ['Arvo', 'serif'],
-		},
-		screens: {
-		'xs': '480',
-		'sm': '767',
-		'md': '960',
-		'lg': '1200',
-		'xl': '1440',
-		'2xl': '1536px',
-		},
-		extend: {},
-	},
-	variants: {},
-	plugins: [],
+  content: [
+    "./index.html",
+    "./partials/**/*.html",
+    "./stylesheets/**/*.scss",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000', // Black
+        secondary: '#FFFFFF', // White
+        accent: {
+          DEFAULT: '#FF6C3C', // Example gradient start (orange)
+          dark: '#FC3854', // Example gradient end (red)
+        },
+      },
+      // Add any additional customizations here
+    },
+  },
+  plugins: [],
 }
